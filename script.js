89dbd8c636ccc72214944e32639e6a7662c297b8
Ihar_Lapau <Freedom7day@yandex.by>
2021-08-04 16:20:35 +0300

    p.125

--- script.js
+++ script.js
@@ -7,20 +7,32 @@
 //   console.log("Что ж, не проблема. Все равно ты молодец!");
 // }
 // alert("Это сдорова!");
+
 var words = ["программа", "макака", "прекресный", "оладушек"];
 var word = words[Math.floor(Math.random() * words.length)];
+/////////////////////////////////////////////////////////////////
 var answersArray = [];
 for (var i = 0; i < word.length; i++) {
   answersArray[i] = "_";
 }
 var remainingLetters = word.length;
-alert(answersArray.join(""));
-var guess = prompt("Угадайте букву или нажимите Отмена для выхода из игры");
-if (guess===null){
-  break;
-}else if(guess.length !==1){
-  alert('Пожалусто, введите только одну букву.');
-} else{
-  //обновляем состояние игры
-  for
+////////////////////////////////////////////////////////////////
+while (remainingLetters > 0) {
+  alert(answersArray.join(" "));
+  var guess = prompt("Угадайте букву или нажимите Отмена для выхода из игры");
+  if (guess === null) {
+    break;
+  } else if (guess.length !== 1) {
+    alert("Пожалусто, введите только одну букву.");
+  } else {
+    //обновляем состояние игры
+    for (var j = 0; j < word.length; j++) {
+      if (word[j] === guess) {
+        answersArray[j] = guess;
+        remainingLetters--;
+      }
+    }
+  }
 }
+// alert(answersArray.join(" "));
+alert("Отлично! Было загадано слово " + "'" + word + "'");
