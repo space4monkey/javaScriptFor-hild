bb47c60be5154414c7c258ba37bc70f043f88149
Ihar_Lapau <Freedom7day@yandex.by>
2021-08-06 13:05:07 +0300

    decision

--- script.js
+++ script.js
@@ -16,23 +16,34 @@ for (var i = 0; i < word.length; i++) {
   answersArray[i] = "_";
 }
 var remainingLetters = word.length;
+var chanse = word.length + 3;
 ////////////////////////////////////////////////////////////////
-while (remainingLetters > 0) {
+while (remainingLetters > 0 && chanse > 0) {
   alert(answersArray.join(" "));
   var guess = prompt("Угадайте букву или нажимите Отмена для выхода из игры");
+  guess = guess.toLowerCase();
+  chanse--;
+  console.log(chanse);
+
   if (guess === null) {
     break;
+  } else if (chanse == 2) {
+    alert("осталась одна попытка.");
   } else if (guess.length !== 1) {
     alert("Пожалусто, введите только одну букву.");
   } else {
     //обновляем состояние игры
+
     for (var j = 0; j < word.length; j++) {
-      if (word[j] === guess) {
+      if (word[j] === guess && answerArray[j] === "_") {
         answersArray[j] = guess;
-        remainingLetters--;
+        if (answersArray) {
+          remainingLetters--;
+        }
       }
     }
   }
 }
+
 // alert(answersArray.join(" "));
 alert("Отлично! Было загадано слово " + "'" + word + "'");
