87fc8d5d87e7366f4e92c9e73282b43528f1f6f8
Ihar_Lapau <37449838+space4monkey@users.noreply.github.com>
2022-03-23 16:25:39 +0300

    p.155 #1

--- repeat/app_145-155.js
+++ repeat/app_145-155.js
@@ -23,6 +23,29 @@
 //Цепной вызов и анимация на jQuery
 // .fadeOut() - постепеннное ослабление*
 // .fadeIn() - постепенное уселение*
-$("#main-he1").text("Этот текст скоро исчезнет").fadeOut(3000); // .text() заменяет текст
-$("#main-he2").fadeOut(3000).fadeIn(2000); // цепная анимация
-$("#main-he3").slideUp(1000).slideDown(1000);
+// $("#main-he1").text("Этот текст скоро исчезнет").fadeOut(3000); // .text() заменяет текст
+// $("#main-he2").fadeOut(3000).fadeIn(2000); // цепная анимация
+// $("#main-he3").slideUp(1000).slideDown(1000);
+// $("#main-he4").fadeIn(3000);
+// $("#main-he5").slideUp(1000).slideDown(1000);
+// $("#main-he6").fadeOut(1000);
+// $("#main-he6").fadeIn(1000);
+
+// for (var i = 0; i < 5; i++) {
+//   $("#main-he7").slideUp(1000).slideDown(1000);
+// }
+// $("#main-he8").show(1000).hide(1000);
+
+//Упражнения
+//#1. Перечисо\лить своих друзей (и сделать из лудшими!)
+var myFrands = ["Дима", "Максим", "Миша", "Наташа", "Павел"];
+
+for (var i = 0; i < myFrands.length; i++) {
+  $("body").append("<p>" + myFrands[i] + "</p>");
+}
+
+$("#main-he1").text("Мои друзья");
+$("p").hide().fadeIn(3000);
+$("p").append(" лучший!");
+
+//#2. Мигающий заголовок
